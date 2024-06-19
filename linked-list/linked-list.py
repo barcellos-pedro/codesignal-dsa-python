31d5091a55a5264e5070a4cf1949001e33553a1d
Pedro Barcellos <pedrobarcellosdosreis@gmail.com>
2024-06-18 21:23:36 -0300

    Update LinkedList

--- linked-list/linked-list.py
+++ linked-list/linked-list.py
@@ -21,30 +21,49 @@ class LinkedList:
 
     def delete(self, value):
         previous_node = None
-        node = self.head
+        current_node = self.head
 
-        if not node == None and node.value == value:
-            self.head = node.next
-            node = None
+        if current_node and current_node.value == value:
+            self.head = current_node.next
+            current_node = None
             return
 
-        while not node == None:
-            if node.value == value:
+        while current_node:
+            if current_node.value == value:
                 break
-            previous_node = node
-            node = node.next
+            previous_node = current_node
+            current_node = current_node.next
 
-        if node == None:
+        if current_node == None:
             return
         
-        previous_node.next = node.next
-        node = None
+        if self.tail == current_node:
+            self.tail = previous_node
 
-    def print(self):
+        previous_node.next = current_node.next
+        current_node = None
+
+    def print(self, reverse=False):
+        if reverse:
+            self.print_reverse()
+            return
+        
         node = self.head 
-        while not node == None:
-            print(node.value, end=" => ")
+        while node:
+            print(node.value, end=" => " if node.next else "\n")
             node = node.next 
+    
+    def print_reverse(self):
+        stack = []
+        current_node = self.head
+
+        while current_node:
+            stack.append(current_node.value)
+            current_node = current_node.next
+        
+        while stack:
+            node = stack.pop()
+            print(node, end=" => " if stack else "\n")
 
 
 my_list = LinkedList()
@@ -53,7 +72,9 @@ my_list.insert('Task B')
 my_list.insert('Task C')
 
 my_list.print()
-my_list.delete('Task B')
-
-print()
+my_list.delete('Task C')
 my_list.print()
+
+print("Reverse traversal")
+my_list.print(reverse=True)
+my_list.print_reverse()
